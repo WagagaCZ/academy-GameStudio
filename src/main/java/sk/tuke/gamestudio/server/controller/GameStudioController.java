52f66d383c061f0dc18701484e08d6c8b5267ded
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-03-21 13:57:24 +0100

    tailwind

--- src/main/java/sk/tuke/gamestudio/server/controller/GameStudioController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/GameStudioController.java
@@ -1,2 +1,14 @@
-package sk.tuke.gamestudio.server.controller;public class GameStudioController {
+package sk.tuke.gamestudio.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GameStudioController {
+
+    @RequestMapping("/")
+    public String mainPage(){return "index";}
+
+    @RequestMapping("/mines")
+    public String minesPage(){return "mines";}
 }
