501959ae38dae2a15829367e71c70dd662fc8f67
WagagaCZ <chalmat1458@gmail.com>
2023-03-29 11:17:59 +0200

    Black Jack - Better graphics

--- src/main/java/sk/tuke/gamestudio/client/game/blackjack/core/Card.java
+++ src/main/java/sk/tuke/gamestudio/client/game/blackjack/core/Card.java
@@ -20,9 +20,54 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card{" +
-                "id=" + id +
-                ", category=" + category +
-                '}';
+        char cid = '0';
+        char iid = '0';
+        StringBuilder finale = new StringBuilder();
+        switch (category){
+            case -1: {
+                return "?";
+            }
+            case 0: {
+                cid = 9824;
+                break;
+            }
+            case 1: {
+                cid = 9829;
+                break;
+            }
+            case 2: {
+                cid = 9827;
+                break;
+            }
+            case 3: {
+                cid = 9830;
+                break;
+            }
+        }
+        finale.append(cid);
+        switch (id){
+            case 1: {
+                iid = 65;
+                break;
+            }
+            case 11: {
+                iid = 74;
+                break;
+            }
+            case 12: {
+                iid = 81;
+                break;
+            }
+            case 13: {
+                iid = 75;
+                break;
+            }
+            default: {
+                iid = (char) (48+id);
+                break;
+            }
+        }
+        finale.append(iid);
+        return finale.toString();
     }
 }
