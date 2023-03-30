623308a9de164001169dfd0324cdaba548f1ebff
milojezek <milo.jezek@gmail.com>
2023-03-30 10:36:00 +0200

    Prepare Peg and PegColor classes

--- src/main/java/sk/tuke/gamestudio/client/game/mastermind/core/Peg.java
+++ src/main/java/sk/tuke/gamestudio/client/game/mastermind/core/Peg.java
@@ -1,4 +1,41 @@
 package sk.tuke.gamestudio.client.game.mastermind.core;
 
 public class Peg {
+    private PegColor color;
+    private boolean hidden;
+
+    public Peg(PegColor color, boolean hidden) {
+        this.color = color;
+        this.hidden = hidden;
+    }
+
+    public Peg(PegColor color) {
+        this.color = color;
+        this.hidden = false;
+    }
+
+    @Override
+    public String toString() {
+        if (hidden) {
+            return "( ? )";
+        } else {
+            return "( " + color.ordinal() + " )";
+        }
+    }
+
+    public PegColor getColor() {
+        return color;
+    }
+
+    public void setColor(PegColor color) {
+        this.color = color;
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
 }
---
+++ src/main/java/sk/tuke/gamestudio/client/game/mastermind/core/PegColor.java
@@ -0,0 +1,11 @@
+package sk.tuke.gamestudio.client.game.mastermind.core;
+
+public enum PegColor {
+    BLACK,
+    GRAY,
+    WHITE,
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW
+}
