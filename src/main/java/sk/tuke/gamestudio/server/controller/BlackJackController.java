28cdaa653202062dc0f03d5ddd5299f822ddb87d
WagagaCZ <chalmat1458@gmail.com>
2023-04-04 15:42:14 +0200

    Black Jack - Added Services, implemented Score, added Double game feature

--- src/main/java/sk/tuke/gamestudio/server/controller/BlackJackController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/BlackJackController.java
@@ -9,12 +9,16 @@ import org.springframework.web.context.WebApplicationContext;
 import sk.tuke.gamestudio.client.game.blackjack.core.Card;
 import sk.tuke.gamestudio.client.game.blackjack.core.Table;
 import sk.tuke.gamestudio.client.game.blackjack.core.Turn;
+import sk.tuke.gamestudio.common.entity.Score;
 import sk.tuke.gamestudio.common.service.ScoreService;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/blackjack")
 @Scope(WebApplicationContext.SCOPE_SESSION)
-public class BlackJackController {
+public class BlackJackController extends GameController {
     private Table table = new Table();
     @Autowired
     private ScoreService scoreService;
@@ -31,7 +35,7 @@ public class BlackJackController {
         if (table == null) {
             newGame();
         }
-        if (move != null){
+        if (move != null) {
             switch (move) {
                 case 1 -> {
                     table.drawNewCard();
@@ -45,6 +49,9 @@ public class BlackJackController {
 
     @RequestMapping("/new")
     private String newGame() {
+        if (table.getBank() == 0){
+            end();
+        }
         table.setup();
         return "blackjack";
     }
@@ -55,25 +62,64 @@ public class BlackJackController {
         return "blackjack";
     }
 
+    @RequestMapping("/end")
+    private String end() {
+        if (table.getTurn() == Turn.END) {
+            if (userController.isLogged()) {
+                scoreService.addScore(new Score(getGameName(), userController.getLoggedUser(), table.getBank(), new Date()));
+            }
+            table = new Table();
+        }
+        return "blackjack";
+    }
+    @RequestMapping("/double")
+    private String dbl(){
+        table.doubleUp();
+        return "blackjack";
+    }
+
     public Turn getTurn() {
         return table.getTurn();
     }
-    public Card[] getDealersHand(){
+
+    public Card[] getDealersHand() {
         return table.getDealerHand();
     }
-    public Card[] getPlayersHand(){
+
+    public Card[] getPlayersHand() {
         return table.getPlayerHand();
     }
-    public String getCardText(Card card){
+
+    public String getCardText(Card card) {
         return card.toString();
     }
-    public boolean isPlayersTurn(){
+
+    public boolean isPlayersTurn() {
         return table.getTurn() == Turn.PLAYER;
     }
-    public boolean isEnd(){
+
+    public boolean isEnd() {
         return table.getTurn() == Turn.END;
     }
-    public boolean isPlayerWinner(){
+
+    public boolean isPlayerWinner() {
         return table.isPlayerWinner();
+    }
+
+    public String getBank() {
+        return String.valueOf(table.getBank());
+    }
+
+    public String getBet() {
+        return String.valueOf(table.getBet());
+    }
+
+    public boolean canDouble() {
+        return (table.getTurn() == Turn.PLAYER && table.getBank() >= (table.getBet() * 2) && table.getPlayerHand()[2] == null);
+    }
+
+    @Override
+    protected String getGameName() {
+        return "blackjack";
     }
 }
