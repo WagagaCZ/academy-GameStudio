1403ed419f071d83589bda008537a30f2d6308e9
milojezek <milo.jezek@gmail.com>
2023-04-05 09:04:18 +0200

    Prepare Mastermind game logic

---
+++ src/main/java/sk/tuke/gamestudio/client/game/mastermind/GameState.java
@@ -0,0 +1,12 @@
+package sk.tuke.gamestudio.client.game.mastermind;
+
+public enum GameState {
+    /** Playing game. */
+    PLAYING,
+
+    /** Game failed. */
+    FAILED,
+
+    /** Game solved. */
+    SOLVED
+}
