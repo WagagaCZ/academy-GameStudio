a2f57b60cad0b4ed69e5ba861bd1beeb19ed189c
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-04-24 19:51:14 +0200

    comments connect 4

--- src/main/java/sk/tuke/gamestudio/server/controller/ConnectController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/ConnectController.java
@@ -13,12 +13,13 @@ import sk.tuke.gamestudio.common.entity.Score;
 import sk.tuke.gamestudio.common.service.ScoreService;
 
 import java.util.Date;
+import java.util.Formatter;
 import java.util.List;
 
 @Controller
 @RequestMapping("/connect")
 @Scope(WebApplicationContext.SCOPE_SESSION)
-public class ConnectController extends GameController{
+public class ConnectController extends GameController {
 
     private Field field = null;
     private GameState state = GameState.PLAYING;
@@ -30,133 +31,122 @@ public class ConnectController extends GameController{
 
     // /connect
     @RequestMapping
-    public String processUserInput(@RequestParam(required = false) Integer row,@RequestParam(required = false) Integer column){
-
-        startOrUpdateGame(row,column);
+    public String processUserInput(@RequestParam(required = false) Integer row, @RequestParam(required = false) Integer column) {
+        startOrUpdateGame(row, column);
         return "connect";
     }
 
-
     // /connect/new
     @RequestMapping("/new")
-    public String newGame(){
+    public String newGame() {
         startNewGame();
         return "connect";
     }
 
-
-
-    private boolean startOrUpdateGame(Integer row, Integer column){
-        if(field==null){
+    private boolean startOrUpdateGame(Integer row, Integer column) {
+        if (field == null) {
             startNewGame();
         }
 
-        boolean justFinished=false;
+        boolean justFinished = false;
 
-        if(row!=null && column!=null){
+        if (row != null && column != null) {
 
-            state = field.getState();
+            state = field.getState(); //preco mas duplikovany game state, aj tu aj vo field? staci vo field a ak to potrebujes v HTML, tak si tu sprav na to getter
 
-            if(state == GameState.PLAYING){
-                    field.makeMove(column);
+            if (state == GameState.PLAYING) {
+                field.makeMove(column);
             }
 
             state = field.getState();
 
-            if(state!=GameState.PLAYING && state!=GameState.FULL){
-                justFinished=true;
-                if(userController.isLogged()){
-                    scoreService.addScore(new Score("connect",userController.getLoggedUser()+state,field.getScore(), new Date()));
+            if (state != GameState.PLAYING && state != GameState.FULL) {
+                justFinished = true;
+                if (userController.isLogged()) {
+                    scoreService.addScore(new Score( //dlhe riadky treba trochu odenterovat
+                            "connect", //tento string mozes dat ako konstantu - refactor -> introduce constant
+                            userController.getLoggedUser() + state,
+                            field.getScore(),
+                            new Date()));
                 }
             }
         }
 
         return justFinished;
-
     }
 
-    private void startNewGame(){
-        field = new Field(9,9);
+    private void startNewGame() {
+        field = new Field(9, 9);
         state = field.getState();
     }
 
-    public String getCurrentTime(){
+    public String getCurrentTime() {
         return new Date().toString();
     }
-
-
+    
     public Tile[][] getFieldTiles() {
-        if(field==null){
-            return null;
-        }else{
-            return field.getTiles();
-        }
+        return field == null ? null : field.getTiles();
     }
 
-    public boolean isPlaying(){
-        if(field==null){
-            return false;
-        }else{
-            return (field.getState()== GameState.PLAYING);
-        }
+    public boolean isPlaying() {
+        return field != null && field.getState() == GameState.PLAYING;
     }
 
-    public String getHtmlField(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("<table class='minefield'> \n");
-        for (int row = 0; row < field.getRows(); row++) {
-            sb.append("<tr>\n");
-            for (int column = 0; column < field.getCols(); column++) {
-                var tile = field.getTile(row, column);
-                sb.append("<td style='" + getTileStyle(tile) + "'>\n");
-                if(field.getState()==GameState.PLAYING){
-                    sb.append("<a href='/connect?row="+row+"&column="+column+"'>\n");
-                    sb.append("<span>" + getTileText(tile) + "</span>");
-                    sb.append("</a>\n");
-                }else{
-                    sb.append("<span>" + getTileText(tile) + "</span>");
+    public String getHtmlField() {
+        Formatter f = new Formatter();
+        f.format("<table class='minefield'> \n");
+        for (int r = 0; r < field.getRows(); r++) {
+            f.format("<tr>\n");
+
+            for (int c = 0; c < field.getCols(); c++) {
+                var tile = field.getTile(r, c);
+                f.format("<td style='%s'>\n", getTileStyle(tile));
+
+                if (field.getState() == GameState.PLAYING) {
+                    f.format("""
+                            <a href='/conntect?r=%d&collumn=%d' style='line-height: 1em;'>
+                            <span>%s</span>
+                            </a>
+                            """, r, c, getTileText(tile));
+                } else {
+                    f.format("<span>%s</span>", getTileText(tile)); //StringBuilder ani formatter nemaju velmi vyznam, pokial vo vnutri robis konkatenaciu
                 }
-                sb.append("</td>\n");
+
+                f.format("</td>\n");
             }
-            sb.append("</tr>\n");
+            f.format("</tr>\n");
         }
-        sb.append("</table>\n");
-        return sb.toString();
+        f.format("</table>\n");
+        return f.toString();
     }
-
-
+    
     public String getTileText(Tile tile) {
-        switch (tile.getPlayer()) {
-            case 1:
-                return "1";
-            case 2:
-                return "2";
-            case 0:
-                return "0";
-            default:
-                throw new IllegalArgumentException("Unsupported tile state " + tile.getPlayer());
-        }
+        return switch(tile.getPlayer()) {
+            case 1 -> "1";
+            case 2 -> "2";
+            case 0 -> "0";
+            default -> throw new IllegalArgumentException("Unsupported tile state " + tile.getPlayer());
+        };
     }
 
     public String getTileStyle(Tile tile) {
-        if (tile.getPlayer() == 1) {
-            return "background-color: #ff5555;border-radius: 50%;";
-        } else if (tile.getPlayer() == 2) {
-            return "background-color: #5555ff;border-radius: 50%;";
-        } else {
-            return "border-radius: 50%;";
-        }
+        return "background-color: " + getPlayerColor(tile.getPlayer()) 
+                + "; border-radius: 50%"; //namiesto style si vytvor na toto classy v cssku a tu uz len men classy, komunikuje to ten vyznam lepsie :)
     }
 
     public String getPlayingStyle() {
         if (field.getState() != GameState.PLAYING) return "";
-        if (field.getCurrentPlayer() == 1) {
-            return "background-color: #ff5555;border-radius: 10%;";
-        } else if (field.getCurrentPlayer() == 2) {
-            return "background-color: #5555ff;border-radius: 10%;";
-        } else {
-            return "";
-        }
+
+        return "background-color: " + getPlayerColor(field.getCurrentPlayer())
+                + "; border-radius: 10%;";
+    }
+
+    private String getPlayerColor(int player) {
+        return switch(player) {
+            case 1 -> "#ff5555";
+            case 2 -> "#5555ff";
+            default -> "initial";
+        };
     }
 
     public GameState getState() {
